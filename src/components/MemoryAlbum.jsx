ed38fb74bf6be439f68d269904f73ddad2de38a8
StasStoun <stoun.stas@gmail.com>
2026-08-14 14:28:24 +0300

    new fotos for album

--- src/components/MemoryAlbum.jsx
+++ src/components/MemoryAlbum.jsx
@@ -49,6 +49,46 @@ import photo_us_42 from '../assets/linda7.jpg';
 import photo_us_43 from '../assets/linda8.jpg';
 import photo_us_44 from '../assets/paraella.jpg';
 import photo_us_45 from '../assets/conmiamor.jpg';
+import photo_us_46 from '../assets/p1.jpg';
+import photo_us_47 from '../assets/p2.jpg';
+import photo_us_48 from '../assets/p3.jpg';
+import photo_us_49 from '../assets/p4.jpg';
+import photo_us_50 from '../assets/p5.jpg';
+import photo_us_51 from '../assets/p6.jpg';
+import photo_us_52 from '../assets/p7.jpg';
+import photo_us_53 from '../assets/p8.jpg';
+import photo_us_54 from '../assets/p9.jpg';
+import photo_us_55 from '../assets/p10.jpg';
+import photo_us_56 from '../assets/p11.jpg';
+import photo_us_57 from '../assets/p12.jpg';
+import photo_us_58 from '../assets/p13.jpg';
+import photo_us_59 from '../assets/p14.jpg';
+import photo_us_60 from '../assets/p15.jpg';
+import photo_us_61 from '../assets/p16.jpg';
+import photo_us_62 from '../assets/p17.jpg';
+import photo_us_63 from '../assets/p18.jpg';
+import photo_us_64 from '../assets/p19.jpg';
+import photo_us_65 from '../assets/p20.jpg';
+import photo_us_66 from '../assets/p21.jpg';
+import photo_us_67 from '../assets/p22.jpg';
+import photo_us_68 from '../assets/p23.jpg';
+import photo_us_69 from '../assets/p24.jpg';
+import photo_us_70 from '../assets/p25.jpg';
+import photo_us_71 from '../assets/p26.jpg';
+import photo_us_72 from '../assets/p27.jpg';
+import photo_us_73 from '../assets/p28.jpg';
+import photo_us_74 from '../assets/p29.jpg';
+import photo_us_75 from '../assets/p30.jpg';
+import photo_us_76 from '../assets/p31.jpg';
+import photo_us_77 from '../assets/p32.jpg';
+import photo_us_78 from '../assets/p33.jpg';
+import photo_us_79 from '../assets/p34.jpg';
+import photo_us_80 from '../assets/p35.jpg';
+import photo_us_81 from '../assets/p36.jpg';
+import photo_us_82 from '../assets/p37.jpg';
+import photo_us_83 from '../assets/p38.jpg';
+import photo_us_84 from '../assets/p39.jpg';
+import photo_us_85 from '../assets/p40.jpg';
 
 
 
@@ -128,6 +168,46 @@ const categorizedPhotos = {
     { id: 43, src: photo_us_43, caption: '' },
     { id: 44, src: photo_us_44, caption: '❤️' },
     { id: 45, src: photo_us_45, caption: 'Con mi amorcito❤️' },
+    { id: 46, src: photo_us_46, caption: '' },
+    { id: 47, src: photo_us_47, caption: '' },
+    { id: 48, src: photo_us_48, caption: '' },
+    { id: 49, src: photo_us_49, caption: '' },
+    { id: 50, src: photo_us_50, caption: '' },
+    { id: 51, src: photo_us_51, caption: '' },
+    { id: 52, src: photo_us_52, caption: '' },
+    { id: 53, src: photo_us_53, caption: '' },
+    { id: 54, src: photo_us_54, caption: '' },
+    { id: 55, src: photo_us_55, caption: '' },
+    { id: 56, src: photo_us_56, caption: '' },
+    { id: 57, src: photo_us_57, caption: '' },
+    { id: 58, src: photo_us_58, caption: '' },
+    { id: 59, src: photo_us_59, caption: '' },
+    { id: 60, src: photo_us_60, caption: '' },
+    { id: 61, src: photo_us_61, caption: '' },
+    { id: 62, src: photo_us_62, caption: '' },
+    { id: 63, src: photo_us_63, caption: '' },
+    { id: 64, src: photo_us_64, caption: '' },
+    { id: 65, src: photo_us_65, caption: '' },
+    { id: 66, src: photo_us_66, caption: '' },
+    { id: 67, src: photo_us_67, caption: '' },
+    { id: 68, src: photo_us_68, caption: '' },
+    { id: 69, src: photo_us_69, caption: '' },
+    { id: 70, src: photo_us_70, caption: '' },
+    { id: 71, src: photo_us_71, caption: '' },
+    { id: 72, src: photo_us_72, caption: '' },
+    { id: 73, src: photo_us_73, caption: '' },
+    { id: 74, src: photo_us_74, caption: '' },
+    { id: 75, src: photo_us_75, caption: '' },
+    { id: 76, src: photo_us_76, caption: '' },
+    { id: 77, src: photo_us_77, caption: '' },
+    { id: 78, src: photo_us_78, caption: '' },
+    { id: 79, src: photo_us_79, caption: '' },
+    { id: 80, src: photo_us_80, caption: '' },
+    { id: 81, src: photo_us_81, caption: '' },
+    { id: 82, src: photo_us_82, caption: '' },
+    { id: 83, src: photo_us_83, caption: '' },
+    { id: 84, src: photo_us_84, caption: '' },
+    { id: 85, src: photo_us_85, caption: '' },
     
   ],
   "sus_dibujos": [
